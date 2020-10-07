8538eccd37f86bb679be7e46051c38e4c9a70657
Garrett Brown <garrettpaulbrown@gmail.com>
2020-10-07 14:25:02 -0700

    Add layout.erb to files. Co-authored by: Ophelia Swen <ophelia.swen@gmail.com> Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- app.rb
+++ app.rb
@@ -1,3 +1,35 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/city')
+require('./lib/train')
 require('pry')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "train_database"})
+
+get('/') do
+  redirect to('/home')
+end
+
+get('/home') do
+  erb(:index)
+end
+
+get('/trains') do
+  @trains = Train.all
+  erb(:trains)
+end
+
+get('/cities') do
+  @cities = City.all
+  erb(:cities)
+end
+
+get('/trains.new') do
+  erb(:new_train)
+end
+
+get('/cities.new') do
+  erb(:new_city)
+end
